668ed442aa53e0e613c1f1bd8d8c1bbf39d25ba8
Ahmed-mohamed11 <ahmedmohamed0aa349@gmail.com>
2025-04-25 01:49:02 +0300

    updates

--- src/pages/home/products/OurProducts.jsx
+++ src/pages/home/products/OurProducts.jsx
@@ -1,10 +1,11 @@
 import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import image1 from "../../../images/gfx/download.jpeg";
 import image2 from "../../../images/gfx/download (1).jpeg";
 import image3 from "../../../images/gfx/download (2).jpeg";
 import image4 from "../../../images/gfx/download (3).jpg";
+import { Phone } from "lucide-react";
 
 const products = [
     {
@@ -69,28 +70,74 @@ const policyParentVariants = {
 
 export default function OurProducts() {
     const [openIndex, setOpenIndex] = useState(null);
+    const ctaRef = useRef(null);
 
     const toggleAccordion = (index) => {
         setOpenIndex(openIndex === index ? null : index);
     };
 
     return (
-        <section className="bg-gradient-to-b from-blue-50 mt-16 via-white to-blue-100 text-right">
+        <section className="bg-gradient-to-b from-blue-50 mt-16 via-white to-blue-100 text-right" >
             <div className="container space-y-8 mx-auto px-6 md:px-12 font-sans">
                 <motion.div
-                    className="container mx-auto px-6 md:px-12 py-10 text-right"
+                    className="container mx-auto px-6 md:px-12 py-10 flex justify-between items center text-right"
                     initial={{ opacity: 0, y: 50 }}
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ duration: 1 }}
                     viewport={{ once: true }}
                 >
-                    <div className="bg-white shadow-lg rounded-3xl p-6 md:p-10 border border-blue-200">
+                    <div className="bg-white w-1/2 shadow-lg rounded-3xl p-6 md:p-10 border border-blue-200">
                         <h2 className="text-2xl md:text-3xl font-bold text-blue-700 mb-4 text-center">قصتنا</h2>
-                        <p className="text-gray-700 leading-loose text-lg md:text-xl font-medium">
-                            بدأت قصتنا مع الفيبر جلاس و تلبية متطلبات السادة العملاء من تصنيع جميع منتجات الفيبر جلاس التي تخدم السادة العملاء و تصب في مصلحة بلادنا الحبيبة مصر مثل أكشاك الحراسة المصممة بطريقة ديكورية ماهرة لتلبية رغبة العميل و تحتوي شكلاً جمالياً و كذلك قمنا بتصنيع الخزانات بأشكالها و أحجامها المختلفة و استخداماتها المتنوعة و أتممنا رحلتنا في مجالات متنوعة تخدم اقتصاد بلادنا مثل أعمدة الإنارة حيث قمنا بتصنيع أعمدة الإنارة بأشكال ديكورية لتعطي جمالاً على المكان الذي توجد فيه قمنا بعمل قسم خاص لتصنيع أعمدة الإنارة بدأً من أصغر عامود ٦٠ سم حتى ١٢ م أعمدة الحدائق و النوادي و الشوارع و قمنا بعمل تصميمات جديدة للأعمدة و موقع خاص ليعرض منتجاتنا لعملائنا و أصبحنا مصنع مختص لتلبية طلبات العملاء.
+                        <p className="text-gray-700 leading-loose text-md md:text-lg font-medium">
+                            بدأت قصتنا مع الفيبر جلاس و تلبية متطلبات السادة العملاء من تصنيع جميع منتجات الفيبر جلاس التي تخدم السادة العملاء و تصب في مصلحة بلادنا الحبيبة مصر مثل أكشاك الحراسة المصممة بطريقة ديكورية ماهرة لتلبية رغبة العميل و تحتوي شكلاً جمالياً و كذلك قمنا بتصنيع الخزانات بأشكالها و أحجامها المختلفة و استخداماتها المتنوعة و أتممنا رحلتنا في مجالات متنوعة تخدم اقتصاد بلادنا مثل أعمدة الإنارة حيث قمنا بتصنيع أعمدة الإنارة بأشكال ديكورية لتعطي جمالاً على المكان الذي توجد فيه قمنا بعمل قسم خاص لتصنيع أعمدة الإنارة بدأً من أصغر عامود ٦٠ سم حتى ١٢ م أعمدة الحدائق و النوادي و الشوارع و قمنا بعمل تصميمات جديدة للأعمدة و موقع خاص ليعرض منتجاتنا لعملائنا و أصبحنا مصنع مختص لتلبية طلبات العملاء
                         </p>
                     </div>
+                    <div className="bg-white shadow-lg rounded-3xl p-6 md:p-10 border border-blue-200">
+                        <div
+                            dir="rtl"
+                            ref={ctaRef}
+                            className="flex flex-col md:flex-row items-center gap-6 px-6 py-10 bg-white rounded-2xl shadow-xl border"
+                        >
+                            <button className="bg-red-600 hover:bg-red-700 text-white  p-3 rounded-lg font-bold shadow-md transition duration-300">
+                                اطلب منتجاتنا
+                            </button>
+                            <p className="text-sm font-medium text-gray-700 text-center md:text-right">
+                                لطلب منتجات شركة الرواد للفيبرجلاس
+                                <br />
+                                اكشاك حراسة - اكوابارك - ألعاب أطفال - خزانات - كرافانات - كبائن سيارات - طفطف
+                            </p>
+                        </div>
+                        <div className="mt-16 grid grid-cols-2 gap-4">
+                            <Link to="/products">
+                                <motion.button
+                                    whileHover={{ scale: 1.05 }}
+                                    whileTap={{ scale: 0.95 }}
+                                    className="w-full bg-blue-700 hover:bg-blue-800 text-white py-3 rounded-xl font-bold shadow-lg transition-all duration-300"
+                                >
+                                    منتجات الشركة
+                                </motion.button>
+                            </Link>
+
+                            <a
+                                href="https://wa.me/+201010195337"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="w-full"
+                            >
+                                <motion.button
+                                    whileHover={{ scale: 1.05 }}
+                                    whileTap={{ scale: 0.95 }}
+                                    className="w-full border border-blue-700 text-blue-700 hover:bg-blue-100 py-3 rounded-xl font-bold shadow-md transition-all duration-300 flex items-center justify-center gap-2"
+                                >
+                                    <Phone className="h-5 w-5" />
+                                    اتصل بنا
+                                </motion.button>
+                            </a>
+                        </div>
+
+                    </div>
                 </motion.div>
+
                 <motion.h4
                     className="text-center md:text-3xl font-extrabold text-red-700 tracking-wide"
                     variants={fadeInUp}
@@ -131,9 +178,9 @@ export default function OurProducts() {
                     ))}
                 </motion.div>
 
-                 <div className="mt-20 grid grid-cols-1 md:grid-cols-2 gap-10">
+                <div className="mt-20 grid grid-cols-1 md:grid-cols-2 gap-10">
 
-                     <motion.div
+                    <motion.div
                         className="bg-white rounded-3xl shadow-xl border border-blue-100 overflow-hidden"
                         initial={{ opacity: 0, scale: 0.95 }}
                         whileInView={{ opacity: 1, scale: 1 }}
@@ -177,9 +224,9 @@ export default function OurProducts() {
                         </motion.div>
                     </motion.div>
 
-                     <motion.div
+                    <motion.div
                         className="bg-white rounded-3xl shadow-xl border border-blue-100 overflow-hidden"
-                         initial={{ opacity: 0, scale: 0.95 }}
+                        initial={{ opacity: 0, scale: 0.95 }}
                         whileInView={{ opacity: 1, scale: 1 }}
                         transition={{ duration: 1 }}
                         viewport={{ once: false, amount: 0.5 }}
